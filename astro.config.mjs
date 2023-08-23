af4d1f6b8430f6ded1a875b23bbe8bd83526e70c
tyler-harpool <102192378+tyler-harpool@users.noreply.github.com>
2023-08-22 21:04:04 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from 'astro/config';
 
 
 // https://astro.build/config
